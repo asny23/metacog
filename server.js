815a8e9b330f425277f6c4039e96219e64566ee6
asny23 <asny23+github@gmail.com>
2021-12-12 18:12:38 +0900

    implement scraping api

--- server.js
+++ server.js
@@ -1,10 +1,51 @@
 import { App } from '@tinyhttp/app'
+import got from 'got'
+import metascraper from 'metascraper'
+import metascraper_author from 'metascraper-author'
+import metascraper_date from 'metascraper-date'
+import metascraper_description from 'metascraper-description'
+import metascraper_image from 'metascraper-image'
+import metascraper_lang from 'metascraper-lang'
+import metascraper_logo from 'metascraper-logo'
+import metascraper_publisher from 'metascraper-publisher'
+import metascraper_title from 'metascraper-title'
+import metascraper_url from 'metascraper-url'
 
 const port = process.env.PORT || 3000
 
-const app = new App()
+const scraper = metascraper([
+  metascraper_author(),
+  metascraper_date(),
+  metascraper_description(),
+  metascraper_image(),
+  metascraper_lang(),
+  metascraper_logo(),
+  metascraper_publisher(),
+  metascraper_title(),
+  metascraper_url()
+])
 
-app.get('/', (_, res) => res.send('<h1>Hello World</h1>'))
+const app = new App({
+  settings: { xPoweredBy: false }
+})
+
 app.get('/health', (_, res) => res.send('ok!'))
+
+app.get('/', async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+
+  const target = req.query.url?.toString()
+  if (!target) {
+    res.status(400).send({ message: 'Please supply an URL to be scraped in the url query parameter.' })
+  }
+  try {
+    const { body: html, url } = await got(target)
+    const metadata = await scraper({ html, url })
+    res.send(metadata)
+  } catch (err) {
+    console.log('Error occured during scraping:', err)
+    res.status(400).send({ message: `Scraping the open graph data from "${target}" failed.` })
+  }
+})
 
 app.listen(port)
